d35bc3a5d54f8de74b2576bd81820655b31b6dcd
Robert Allen Krause <robert.allen.krause@gmail.com>
2014-03-26 18:19:19 -0500

    - Added jellybeans, points and weather.

--- ircbot.el
+++ ircbot.el
@@ -69,3 +69,48 @@
 (defun source-command (text)
   "https://github.com/sauerkrause/bot.emacs")
 (puthash "source" 'source-command command-table)
+
+(require 'web)
+(defun spit-page (fn url)
+  (web-http-get (lambda (httpc header page-data)
+		  (funcall fn page-data))
+		:url url))
+(defun points-command (fn text)
+  (let* ((victim (car (split-string text)))
+	 (url (format "http://localhost:8000/%s/points" victim)))
+    (spit-page fn url)))
+(puthash "points" 'points-command async-command-table)
+
+(defun jellybeans-command (fn text)
+  (let* ((victim (car (split-string text)))
+	 (url (format "http://localhost:8000/%s/jellybeans" victim)))
+    (spit-page fn url)))
+(puthash "jellybeans" 'jellybeans-command async-command-table)
+
+(require 'dom)
+(require 'web)
+
+(defun xml-from-body (body)
+  (with-temp-buffer
+    (insert body)
+    (xml-parse-region (point-min) (point-max))))
+
+(defun get-weather-description (xml)
+  (format "%s and %s C at %s"
+	  (get-weather-attribute 'weather xml)
+	  (get-weather-attribute 'temp_c xml)
+	  (get-weather-attribute 'location xml)))
+
+(defun get-weather-attribute (sym xml)
+  (let ((tmp-node
+          (car (xml-get-children (car xml)
+                                 sym))))
+    (car (cddr tmp-node))))
+(defun weather-command (fn text)
+  (let* ((station (car (split-string text)))
+	 (url (format "http://w1.weather.gov/xml/current_obs/%s.xml" station)))
+    (message "%s" url)
+    (web-http-get (lambda (httpc headers body)
+		    (funcall fn (get-weather-description (xml-from-body body))))
+		  :url url)))
+(puthash "weather" 'weather-command async-command-table)
